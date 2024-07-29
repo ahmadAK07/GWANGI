146fa12f0747fccbb9933de320ea9a8725192be3
ahmadAK07 <muhammadahmadamin512@gmail.com>
2024-07-29 13:14:23 +0500

    working

--- src/js/parallax.js
+++ src/js/parallax.js
@@ -1,19 +1,17 @@
-// import SimpleParallax from "../../node_modules/simple-parallax-js/dist/vanilla";
-// console.log(SimpleParallax)
+
 var image = document.querySelectorAll(".image-parallax");
-// new simpleParallax(image[0], {
-//     orientation:"right"
-import SimpleParallax from "./vanilla/simpleParallaxVanilla.es.js";
 
-image.forEach((img)=>{
-    new SimpleParallax(img)
-})
-window.addEventListener("scroll", ()=>{
-    document.querySelectorAll(".simple-parallax-initialized").forEach((item)=>{
-        item.style.overflowY = "visible" ;
-        item.style.overflowX = "visible" ;
+// import SimpleParallax from "./vanilla/simpleParallaxVanilla.es.js";
 
-    })
+// image.forEach((img)=>{
+//     new SimpleParallax(img)
+// })
+// window.addEventListener("scroll", ()=>{
+//     document.querySelectorAll(".simple-parallax-initialized").forEach((item)=>{
+//         item.style.overflowY = "visible" ;
+//         item.style.overflowX = "visible" ;
+
+//     })
 
 
-})
+// })
